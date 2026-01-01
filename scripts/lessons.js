3347d51a4011a48ce7f2a90a77fcb40659b55e09
Leon Žlender <jsjjjs6066@outlook.com>
2026-01-01 22:23:46 +0100

    feat: Introduce interactive code explainer components, add color support to hoverable code, and update theme colors and various UI styles.

--- scripts/lessons.js
+++ scripts/lessons.js
@@ -12,16 +12,19 @@ class HoverableCode extends HTMLElement {
         super();
         this.originalText = '';
         this.enterText = '';
+        this.color = '';
     }
 
     static get observedAttributes() {
-        return ['enter'];
+        return ['enter', 'color'];
     }
 
     connectedCallback() {
         // Wait for the parser to create the children
         setTimeout(() => {
             if (!this.originalText) {
+                this.style.color = this.color;
+                this.style.borderColor = this.color;
                 this.originalText = this.innerHTML;
                 this.render();
                 this.addEventListeners();
@@ -32,6 +35,9 @@ class HoverableCode extends HTMLElement {
     attributeChangedCallback(name, oldValue, newValue) {
         if (name === 'enter' && oldValue !== newValue) {
             this.enterText = newValue;
+        }
+        if (name === 'color' && oldValue !== newValue) {
+            this.color = newValue;
         }
     }
 
@@ -51,12 +57,171 @@ class HoverableCode extends HTMLElement {
         if (this.innerHTML !== this.originalText) {
             this.innerHTML = this.originalText;
         }
-        this.style.minWidth = String(this.clientWidth-10) + 'px';
-        this.style.minHeight = String(this.clientHeight-10) + 'px';
+        this.style.minWidth = String(this.clientWidth - 10) + 'px';
+        this.style.minHeight = String(this.clientHeight - 10) + 'px';
+    }
+}
+
+class Explainer extends HTMLElement {
+    constructor() {
+        super();
+        this.target;
+        this.infos;
+    }
+
+    setCallbacks(element) {
+        element.addEventListener('mouseover', () => {
+            let siblings = element.parentNode.children;
+            for (let i = 0; i < siblings.length; i++) {
+                if (siblings[i] == element) {
+                    continue;
+                }
+                siblings[i].style.color = 'transparent';
+                siblings[i].style.backgroundColor = 'transparent';
+            }
+            let c = document.createElement('div');
+            c.innerText = element.content;
+            c.id = 'info-content';
+            c.style.color = element.color;
+            c.style.paddingLeft = '10px';
+            element.after(c);
+        });
+        element.addEventListener('mouseleave', () => {
+            let siblings = element.parentNode.children;
+            let infoI = 0;
+            for (let i = 0; i < siblings.length; i++) {
+                if (siblings[i] == element) {
+                    infoI++;
+                    continue;
+                }
+                if (siblings[i].className != 'info') {
+                    continue;
+                }
+                let color = this.infos[infoI].color;
+                if (color == '') {
+                    color = primaryColor;
+                }
+                siblings[i].style.color = color;
+                siblings[i].style.backgroundColor = color;
+                infoI++;
+            }
+            document.getElementById('info-content').remove();
+        });
+    }
+
+    connectedCallback() {
+        setTimeout(() => {
+            this.target = this.previousElementSibling;
+            while (this.target) {
+                if (this.target.tagName.toLowerCase() == 'pre') {
+                    break;
+                }
+                this.target = this.target.previousElementSibling;
+            }
+            this.render();
+        });
+    }
+
+    render() {
+        this.className = 'explainer-cont';
+        let infos = Array.from(this.getElementsByTagName('explainer-info'));
+        infos.forEach((info) => {
+            info.content = info.innerHTML;
+        });
+        infos.sort((a, b) => {
+            return a.pos - b.pos;
+        });
+        this.infos = infos;
+
+        this.innerHTML = '';
+
+
+        if (infos.length == 0) {
+            return;
+        }
+
+        let prevInfo = infos[0];
+        let margin = document.createElement('div');
+        margin.className = 'margin';
+        margin.innerText = this.target.innerText.slice(0, Number(prevInfo.pos));
+        this.appendChild(margin);
+        let code = document.createElement('div');
+        code.className = 'info'
+        if (prevInfo.color == "") {
+            prevInfo.color = primaryColor;
+        }
+        code.style.color = prevInfo.color;
+        code.style.backgroundColor = prevInfo.color;
+        code.innerText = this.target.innerText.slice(Number(prevInfo.pos), Number(prevInfo.pos) + Number(prevInfo.size));
+        code.content = prevInfo.content;
+        code.color = prevInfo.color;
+        code.style.marginLeft = String(prevInfo.additionalPadding) + 'px';
+        this.appendChild(code);
+        this.setCallbacks(code);
+        for (let i = 1; i < infos.length; i++) {
+            let info = infos[i];
+            let margin = document.createElement('div');
+            margin.className = 'margin';
+            margin.innerText = this.target.innerText.slice(Number(prevInfo.pos) + Number(prevInfo.size), Number(info.pos));
+            this.appendChild(margin);
+            let code = document.createElement('div');
+            code.className = 'info';
+            if (info.color == "") {
+                info.color = primaryColor;
+            }
+            code.style.color = info.color;
+            code.style.backgroundColor = info.color;
+            code.innerText = this.target.innerText.slice(Number(info.pos), Number(info.pos) + Number(info.size));
+            code.content = info.content;
+            code.color = info.color;
+            code.style.marginLeft = String(info.additionalPadding) + 'px';
+            this.appendChild(code);
+            this.setCallbacks(code);
+            prevInfo = info;
+        }
+    }
+}
+
+class ExplainerInfo extends HTMLElement {
+    constructor() {
+        super();
+        this.color = '';
+        this.pos = 0;
+        this.size = 0;
+        this.content = '';
+        this.additionalPadding = 0;
+    }
+
+    static get observedAttributes() {
+        return ['color', 'pos', 'size', 'hc-paddings'];
+    }
+
+    connectedCallback() {
+        // Wait for the parser to create the children
+        setTimeout(() => {
+            this.content = this.innerHTML;
+        });
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'color' && oldValue !== newValue) {
+            this.color = newValue;
+        }
+        if (name === 'pos' && oldValue !== newValue) {
+            this.pos = newValue;
+        }
+        if (name === 'size' && oldValue !== newValue) {
+            this.size = newValue;
+        }
+        if (name === 'hc-paddings' && oldValue !== newValue) {
+            this.additionalPadding = Number(newValue) * 6.67;
+        }
     }
 }
 
 customElements.define('hoverable-code', HoverableCode);
+customElements.define('explainer-cont', Explainer);
+customElements.define('explainer-info', ExplainerInfo);
 
 function completeLesson(lesson) {
     localStorage.setItem(lesson, "da");
@@ -77,9 +242,8 @@ function main() {
             }
         };
     }
-    
+
     let hoverableCodes = document.getElementsByClassName("hoverable-code");
-    console.log(hoverableCodes);
     for (let i = 0; i < hoverableCodes.length; i++) {
         hoverableCodes[i].style.minWidth = String(hoverableCodes[i].clientWidth - 10) + 'px';
         hoverableCodes[i].style.minHeight = String(hoverableCodes[i].clientHeight - 10) + 'px';
@@ -91,5 +255,18 @@ function main() {
         val.firstElementChild.firstElementChild.lastElementChild.style.borderTopRightRadius = "10px";
         val.lastElementChild.lastElementChild.firstElementChild.style.borderBottomLeftRadius = "10px";
         val.lastElementChild.lastElementChild.lastElementChild.style.borderBottomRightRadius = "10px";
+    });
+
+    let stare_gluposti = Array.from(document.querySelectorAll("p"));
+    stare_gluposti.forEach((val, i, a) => {
+        if (val.style.whiteSpace == 'pre-wrap') {
+            val.style.fontFamily = "Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace";
+            val.style.fontSize = "1.5em";
+            val.style.paddingLeft = "2px";
+            for (let line of val.innerText.split("\n")) {
+                let newLine = "  " + line;
+                val.innerText = val.innerText.replace(line, newLine);
+            }
+        }
     });
 }
