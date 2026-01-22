5afb7078f266a6c30e9835210643ea3afe98f9eb
Leon Žlender <jsjjjs6066@outlook.com>
2026-01-22 13:02:46 +0100

    feat: add advanced for loop lesson and update index page to include its link.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
-const lessons = ["ispis", "varijable", "operatori", "string", "usporedjivanje", "if", "input", "liste-osnove", "liste-elementi", "liste-dodavanje", "for-osnove"];
+const lessons = ["ispis", "varijable", "operatori", "string", "usporedjivanje", "if", "input", "liste-osnove", "liste-elementi", "liste-dodavanje", "for-osnove", "for-napredno"];
+const version = "1-liste";
+let last_version = version;
 
 function get_lessons() {
     lessons.forEach((lesson) => {
@@ -16,4 +18,12 @@ function check_section(section) {
     }
 }
 
+function update_version() {
+    if (localStorage.getItem("version") !== version) {
+        last_version = localStorage.getItem("version");
+        localStorage.setItem("version", version);
+    }
+}
+
+update_version();
 get_lessons();
